ac6b6b9b14979be7875835698cbc5a491b575c8f
nator333 <m.nakamata35@gmail.com>
2019-04-11 17:59:29 -0700

    Update header nav

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,12 +7,12 @@ library.add(fas);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
