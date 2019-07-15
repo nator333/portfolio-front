67a16455e972981364af18ef19bea0e7db3696d4
nator333 <m.nakamata35@gmail.com>
2019-07-14 23:00:56 -0700

    Update works

--- front/src/app/works/works.component.ts
+++ front/src/app/works/works.component.ts
@@ -34,35 +34,35 @@ export class WorksComponent implements OnInit {
 
   projects = [
     new Project(
-      'Longman Dictionary Bubble',
+      'Silver Bullet',
+      'Java Group Project',
+      'Original 2 Player Desktop Game made by JavaFX, JDK12. Go check & please try in your PC!',
+      '/assets/projects/SilverBullet.png',
+      'https://github.com/cornerstone18aug/silver-bullet'),
+    new Project(
+      'Longman Bubble',
       'Chrome Extension',
-      'For myself and other ESL students. By using pure JavaScript. English-English dictionary pop-up.',
+      'English-English dictionary pop-up for all ESL people, using pure JavaScript.',
       '/assets/projects/longman.png',
       'https://chrome.google.com/webstore/detail/longman-dictionary-bubble/cajklhanpcgcpkikgpcnogpdndpjdjjn?hl=en'),
     new Project(
       'ElementsCPR Website',
       'Corporate Website',
-      'Used Kusanagi(WordPress) with Docker & Docker Compose on GCE. Applied a public theme.',
+      'Used Kusanagi framework(WordPress) with Docker on GCE using a public theme.',
       '/assets/projects/elementscpr.png',
       'https://elementscpr.com'),
     new Project(
+      'Nakamata.Tech',
       'Portfolio Website',
-      'Single Page Application',
-      'Built by Angular7, Bulma, Ktor, MySQL. Hosted by Firebase Hosting.',
+      'Built by Angular7, Bulma, Hosted by Firebase Hosting.',
       '/assets/projects/portfolio.png',
       ''),
     new Project(
-      'Sumica',
+      'Bookmanager',
       'iOS Group Project',
-      'Work in progress. Aimed for communication enhancement for room share.',
+      'Organizing Bookmarks using AI (Natural Language Processing API). Releasing on AppStore by August',
       '/assets/projects/WIP.png',
       ''),
-    new Project(
-      'Be A Book Lover',
-      'Android Personal Project',
-      'Work in progress. It records reading book and offers statistic reporting to motivate reading books more.',
-      '/assets/projects/WIP.png',
-      '')
   ];
 
   constructor() {
